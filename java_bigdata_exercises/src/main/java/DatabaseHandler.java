47304a3c49f141c5cd229178c00e8132b48931bf
Eric Mangin <ericpaulmangin@gmail.com>
2023-07-11 10:34:22 -0700

    "Updated 'main()' method, finished CRUD command methods. Began improving exception handling with more specifics."

--- java_bigdata_exercises/src/main/java/DatabaseHandler.java
+++ java_bigdata_exercises/src/main/java/DatabaseHandler.java
@@ -10,7 +10,7 @@ public class DatabaseHandler {
         String url = "jdbc:postgresql://localhost/test";
         Properties props = new Properties();
         System.out.println("USER: " + "postgres");
-        System.out.println("DB_PASSWORD: " + System.getenv("DB_PASSWORD"));
+        System.out.println("DB_PASSWORD: " + "••••••••••");
         props.setProperty("password", System.getenv("DB_PASSWORD"));
 
         props.setProperty("user", "postgres");
@@ -31,6 +31,8 @@ public class DatabaseHandler {
         6. Integration with a Web Framework - Once data access layer is solid, begin to
                 build a web interface for my app using the Java web framework, Spring Boot.
      */
+
+    // Create (from CRUD) Command
     public void addStudent(String name, int age, String email, String course) {
         String query = "INSERT INTO students(name, age, email, course) VALUES(?, ?, ?, ?)";
         try {
@@ -41,10 +43,19 @@ public class DatabaseHandler {
             pstmt.setString(4, course);
             pstmt.executeUpdate();
         } catch (SQLException e) {
-            e.printStackTrace();
+            if (e instanceof SQLIntegrityConstraintViolationException) {
+                System.out.println("Error: A student with the same ID already exists.");
+            } else if (e instanceof SQLDataException) {
+                System.out.println("Error: Invalid data format.");
+            } else {
+                System.out.println("An error occurred while adding the student.");
+                e.printStackTrace();
+            }
         }
     }
 
+
+    // Read (from CRUD) command
     public List<Student> getStudents() {
         List<Student> students = new ArrayList<>();
         String query = "SELECT * FROM students";
@@ -65,11 +76,58 @@ public class DatabaseHandler {
         return students;
     }
 
+    // Update (from CRUD) command
+    public void updateStudent(int id, String name, int age, String email, String course) {
+        String query = "UPDATE students SET name = ?, age = ?, email = ?, course = ? WHERE id = ?";
+        try {
+            PreparedStatement pstmt = conn.prepareStatement(query);
+            pstmt.setString(1, name);
+            pstmt.setInt(2, age);
+            pstmt.setString(3, email);
+            pstmt.setString(4, course);
+            pstmt.setInt(5, id);
+            pstmt.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void updateStudentEmail(int id, String newEmail) {
+        String query = "UPDATE students SET email = ? WHERE id = ?";
+        try {
+            PreparedStatement pstmt = conn.prepareStatement(query);
+            pstmt.setString(1, newEmail);
+            pstmt.setInt(2, id);
+            pstmt.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    // Delete (from CRUD) command
+    public void deleteStudent(int id) {
+        String query = "DELETE FROM students WHERE id = ?";
+        try {
+            PreparedStatement pstmt = conn.prepareStatement(query);
+            pstmt.setInt(1, id);
+            pstmt.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
 
     public static void main(String[] args) {
         DatabaseHandler dbHandler = new DatabaseHandler();
         dbHandler.connect();
-        dbHandler.addStudent("John Doe", 22, "john.doe@example.com", "Computer Science");
+        // comment this out for now, to prevent addt'l 'John Doe's until code amended
+//      dbHandler.addStudent("John Doe", 22, "john.doe@example.com", "Computer Science");
+
+        dbHandler.updateStudent(1, "John Doe", 22, "john.doe@example.com", "Data Science");
+        dbHandler.updateStudentEmail(2, "new.email@example.com");
+
+        // commented out for now, so Students don't continually get deleted.
+//        dbHandler.deleteStudent(1);
 
         List<Student> students = dbHandler.getStudents();
         for (Student student : students) {
