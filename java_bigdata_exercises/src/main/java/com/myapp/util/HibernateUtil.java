6897c5bbbacca86163923ca75c321784fdc70d99
Eric Mangin <ericpaulmangin@gmail.com>
2023-07-14 01:04:50 -0700

    "Started moving code towards a more interactive application, with the plan to use the console for User input."

--- java_bigdata_exercises/src/main/java/com/myapp/util/HibernateUtil.java
+++ java_bigdata_exercises/src/main/java/com/myapp/util/HibernateUtil.java
@@ -3,25 +3,25 @@ package com.myapp.util;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.hibernate.SessionFactory;
-import org.hibernate.boot.Metadata;
-import org.hibernate.boot.MetadataSources;
-import org.hibernate.boot.registry.StandardServiceRegistry;
-import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
 
 public class HibernateUtil {
+
     private static final SessionFactory sessionFactory = buildSessionFactory();
     private static final Logger logger = LogManager.getLogger(HibernateUtil.class);
 
     private static SessionFactory buildSessionFactory() {
+        // switched this method to a simpler, high-level API with 'Configuration()'
+        // Using the docs' low-level 'StandardServiceRegistry' and 'MetadataSources'
+        // Hibernate APIs was "technically" correct, but also too complex for my purposes here.
         try {
-            StandardServiceRegistry standardRegistry =
-                    new StandardServiceRegistryBuilder().configure("hibernate.cfg.xml").build();
-            Metadata metaData =
-                    new MetadataSources(standardRegistry).getMetadataBuilder().build();
-            return metaData.getSessionFactoryBuilder().build();
-        } catch (Throwable th) {
-            logger.error("Initial SessionFactory creation failed" + th);
-            throw new ExceptionInInitializerError(th);
+            // Create the SessionFactory from hibernate.cfg.xml
+            return new Configuration().configure().buildSessionFactory();
+        } catch (Throwable e) {
+            // Logging the exception
+            logger.error("Initial SessionFactory creation failed.", e);
+            // re-throwing (as I was swallowing the exception before)
+            throw new ExceptionInInitializerError(e);
         }
     }
 
@@ -30,7 +30,9 @@ public class HibernateUtil {
     }
 
     public static void shutdown() {
+        // Close caches and connection pools
         getSessionFactory().close();
     }
 }
+
 
